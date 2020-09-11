960fbf930d0da4ddf79a3e593243487ef526a1cf
Ernesto Hernández <ernesto@nimbo.mx>
2020-09-11 12:19:08 -0500

    class Block

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 import PKG from './package.json';
+import Block from './src/blockchain/block';
 
 const { name, version } = PKG;
 
 console.log(`${name} v${version}`);
 
-console.log('Hola mundo !!');
+const block = new Block(Date.now(), 'pr3v10us-h4Sh', 'h4Sh', 'd4t4');
+console.log(block.toString());
