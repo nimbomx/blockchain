2f748c4229595893e448f8a88a0995ceaac6ed2e
Ernesto Hernández <ernesto@nimbo.mx>
2020-09-15 13:15:57 -0500

    Node connections

--- src/service/index.js
+++ src/service/index.js
@@ -2,11 +2,13 @@ import express from 'express';
 import bodyParser from 'body-parser';
 
 import Blockchain from '../blockchain';
+import P2PService from './p2p';
 
 const { HTTP_PORT = 3001 } = process.env;
 
 const app = express();
 const blockchain = new Blockchain();
+const p2pService = new P2PService(blockchain); 
 
 app.use(bodyParser.json());
 
@@ -25,4 +27,5 @@ app.post('/mine', (req, res) => {
 });
 app.listen( HTTP_PORT, () => {
     console.log(`Service HTTP:${HTTP_PORT} listening...`);
+    p2pService.listen();
 });
