702a7ed2adb1a2ea801b1a8e1b49b404b3fb88a6
Ernesto Hernández <ernesto@nimbo.mx>
2020-09-15 12:54:25 -0500

    Setup p2p

--- src/service/index.js
+++ src/service/index.js
@@ -3,13 +3,26 @@ import bodyParser from 'body-parser';
 
 import Blockchain from '../blockchain';
 
-const { HTTP_PORT = 3000 } = process.env;
+const { HTTP_PORT = 3001 } = process.env;
 
 const app = express();
 const blockchain = new Blockchain();
 
 app.use(bodyParser.json());
 
+blockchain.addBlock('Express');
+app.get('/blocks', (req, res) => {
+    res.json(blockchain.blocks);
+});
+app.post('/mine', (req, res) => {
+    const {body: {data}} = req;
+    const block = blockchain.addBlock(data);
+
+    res.json({
+        blocks: blockchain.blocks.length,
+        block
+    })
+});
 app.listen( HTTP_PORT, () => {
     console.log(`Service HTTP:${HTTP_PORT} listening...`);
 });
